a8a28bbc76ba8140fd448a9e334e77c2ff025f7c
Rifkhi Akbar Rustandi <rakbarputrar@gmail.com>
2025-12-10 15:10:07 +0700

    feat: set flask to 2.2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-__version = "3.1.1"
+__version = "3.1.2"
 
 setup(
     name="oc-dms-mirror",
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         "oc-cdtapi >= 3.29.1",
         "oc-checksumsq >= 10.0.3",
-        "flask==3.1.2",
+        "flask==2.2.5",
         "gunicorn==23.0.0",
         "oc_logging==1.1.7"
     ],
